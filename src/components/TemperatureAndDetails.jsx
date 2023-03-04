cdde52984be02ef1471d74b60953eff768cbc4c2
Jay <116904048+Vegasaga@users.noreply.github.com>
2023-03-04 11:21:43 -0800

    updated img alt

--- src/components/TemperatureAndDetails.jsx
+++ src/components/TemperatureAndDetails.jsx
@@ -16,7 +16,7 @@ function TemperatureAndDetails() {
       <div className="flex flex-row items-center justify-between text-white py-3">
         <img
           src="https://openweathermap.org/img/wn/01d@2x.png"
-          alt="weather image"
+          alt="weather sun"
           className="w-20"
         />
         <p className="text-5xl">34°</p>
