130b7bc3e56dc237cc7595e0ea90c56c2cc2b008
Jay <116904048+Vegasaga@users.noreply.github.com>
2023-03-05 10:41:18 -0800

    updated

--- src/App.js
+++ src/App.js
@@ -20,18 +20,24 @@ function App() {
         <a
           href="https://www.linkedin.com/in/johanna-sanchez-27a0491b1/"
           target="_blank"
+          rel="noreferrer"
         >
           {" "}
           Johanna Sanchez
         </a>{" "}
         and is open-sourced on{" "}
-        <a href="https://github.com/Vegasaga/weather-react" target="_blank">
+        <a
+          href="https://github.com/Vegasaga/weather-react"
+          target="_blank"
+          rel="noreferrer"
+        >
           GitHub
         </a>{" "}
         and hosted on{" "}
         <a
           href="https://aquamarine-cobbler-3f8040.netlify.app/"
           target="_blank"
+          rel="noreferrer"
         >
           Netlify
         </a>
