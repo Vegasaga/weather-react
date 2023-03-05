7524009c0959efc10efa052c8f03f35a9df87473
Jay <116904048+Vegasaga@users.noreply.github.com>
2023-03-05 10:39:41 -0800

    added Github link

--- src/App.js
+++ src/App.js
@@ -14,6 +14,28 @@ function App() {
       <TimeAndLocation />
       <TemperatureAndDetails />
       <Forecast title="Hourly Forecast" />
+      <br />
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/johanna-sanchez-27a0491b1/"
+          target="_blank"
+        >
+          {" "}
+          Johanna Sanchez
+        </a>{" "}
+        and is open-sourced on{" "}
+        <a href="https://github.com/Vegasaga/weather-react" target="_blank">
+          GitHub
+        </a>{" "}
+        and hosted on{" "}
+        <a
+          href="https://aquamarine-cobbler-3f8040.netlify.app/"
+          target="_blank"
+        >
+          Netlify
+        </a>
+      </footer>
     </div>
   );
 }
