19e50e614015846649da1b8f83615d5e2a86be88
Jay <116904048+Vegasaga@users.noreply.github.com>
2023-03-12 04:42:41 -0700

    updated App.js added extensions and functions

--- src/App.js
+++ src/App.js
@@ -5,15 +5,59 @@ import Inputs from "./components/Inputs";
 import TimeAndLocation from "./components/TimeAndLocation";
 import TemperatureAndDetails from "./components/TemperatureAndDetails";
 import Forecast from "./components/Forecast";
+import getFormattedWeatherData from "./services/WeatherService";
+import { useEffect, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
+  const [query, setQuery] = useState({ q: "berlin" });
+  const [units, setUnits] = useState("metric");
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    const fetchWeather = async () => {
+      const message = query.q ? query.q : "current location.";
+
+      toast.info("Fetching weather for " + message);
+      await getFormattedWeatherData({ ...query, units }).then((data) => {
+        toast.success(
+          `Successfully fetched weather for ${data.name}, ${data.country}.`
+        );
+
+        setWeather(data);
+      });
+    };
+
+    fetchWeather();
+  }, [query, units]);
+
+  const formatBackground = () => {
+    if (!weather) return "from-green-400 to-blue-500";
+    const threshold = units === "metric" ? 20 : 60;
+    if (weather.temp <= threshold) return "from-green-400 to-blue-500";
+
+    return "from-yellow-700 to-orange-700";
+  };
+
   return (
-    <div className="mx-auto max-w-screen-md mt-7 py-5 px-32 bg-gradient-to-br from-green-400 to-blue-500 h-fit shadow-xl shadow-gray-400">
-      <TopButtons />
-      <Inputs />
-      <TimeAndLocation />
-      <TemperatureAndDetails />
-      <Forecast title="Hourly Forecast" />
+    <div
+      className={`mx-auto max-w-screen-md mt-7 py-5 px-32 bg-gradient-to-br from-green-400 to-blue-500 h-fit shadow-xl shadow-gray-400 ${formatBackground()}`}
+    >
+      <TopButtons setQuery={setQuery} />
+      <Inputs setQuery={setQuery} units={units} setUnits={setUnits} />
+
+      {weather && (
+        <div>
+          <TimeAndLocation weather={weather} />
+          <TemperatureAndDetails weather={weather} />
+
+          <Forecast title="hourly forecast" items={weather.hourly} />
+          <Forecast title="daily forecast" items={weather.daily} />
+
+          <ToastContainer autoClose={5000} theme="colored" newestOnTop={true} />
+        </div>
+      )}
       <br />
       <footer>
         This project was coded by{" "}
