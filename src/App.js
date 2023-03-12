b7a537d4073bbc917f183bda63d48633ae364805
Jay <116904048+Vegasaga@users.noreply.github.com>
2023-03-12 05:42:45 -0700

    updated the border radius

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ function App() {
 
   return (
     <div
-      className={`mx-auto max-w-screen-md mt-7 py-5 px-32 bg-gradient-to-br from-green-400 to-blue-500 h-fit shadow-xl shadow-gray-400 ${formatBackground()}`}
+      className={`mx-auto max-w-screen-md mt-7 py-5 px-32 rounded-lg bg-gradient-to-br from-green-400 to-blue-500 h-fit shadow-xl shadow-gray-400 ${formatBackground()}`}
     >
       <TopButtons setQuery={setQuery} />
       <Inputs setQuery={setQuery} units={units} setUnits={setUnits} />
