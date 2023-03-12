56ce09fd643168dfabc297ace98bcbbd75671fbe
Jay <116904048+Vegasaga@users.noreply.github.com>
2023-03-12 05:03:28 -0700

    removed loader-spinner

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-  const [query, setQuery] = useState({ q: "berlin" });
+  const [query, setQuery] = useState({ q: "paris" });
   const [units, setUnits] = useState("metric");
   const [weather, setWeather] = useState(null);
 
